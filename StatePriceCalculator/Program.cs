caecbf1bace666050b872e0171b9b99c52e9ed65
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2020-09-11 10:58:06 +0200

    Initial commit

---
+++ StatePriceCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StatePriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
